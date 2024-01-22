86af062d1167288198e00e51140eace1369c942a
hebaabdullah1 <127639276+hebaabdullah1@users.noreply.github.com>
2024-01-23 01:25:36 +0200

    Initial Commit

---
+++ iOS_TextEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_TextEditor
+//
+//  Created by Shimaa Elcc on 23.1.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
